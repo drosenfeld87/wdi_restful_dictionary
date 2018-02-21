99778cb4dda4bb7035c90c41e1af063c1bdb7bb3
debbie <drosenfeld87@gmail.com>
2018-02-21 16:20:26 -0500

    update redirect working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def index
+    render :index
+  end
+
+  def show
+    render :show
+  end
+
+  def new
+    render :new
+  end
+
+  def create
+    redirect_to entries_url
+  end
+
+  def edit
+    render :edit
+  end
+
+  def update
+    redirect_to entry_url(params[:id])
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :entries
+  # resources :entries, except: [:destroy]
+  # resources :entries, only: [:index, :show]
 end
